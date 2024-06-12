70925e4b3ffcb1befe5cdd67d8a1fda82a11806a
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-06-12 04:23:30 +0200

    class for the Jpanel

---
+++ view/GamePanel.java
@@ -0,0 +1,90 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import org.lwjgl.Sys;
+
+import controller.KeyboardController;
+import model.field.Field;
+import model.character.Character;
+
+public class GamePanel extends JPanel implements Runnable {
+    private Thread gameThread;
+    private Field field;
+    private BufferedImage buffer;
+    KeyboardController kbController = new KeyboardController();
+    
+    public GamePanel() {
+        setPreferredSize(new Dimension(1400, 800));
+        setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.setFocusable(true);
+
+        this.field = new Field(1400, 800);
+        field.createSections();
+        field.addCharacter("c1", "resources/c1/");
+    }
+
+    public void startGameThread() {
+        if (gameThread == null) {
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000 / 24;
+        double drawNextTile = System.nanoTime() + drawInterval;
+        while (gameThread != null) {
+            update();
+
+            try {
+                double remainingTime = drawNextTile - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+                Thread.sleep((long)remainingTime);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void update() {
+        field.act(kbController);
+        paintComponent(getGraphics());
+    }
+
+    public void paintCharacter(Graphics g, Character c) {
+        try {
+            BufferedImage img = ImageIO.read(new File(c.getImgPath() + c.getSelectedIMG()));
+            g.drawImage(img, c.getXPosition(), c.getYPosition(), this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        if (buffer == null || buffer.getWidth() != getWidth() || buffer.getHeight() != getHeight()) {
+            buffer = (BufferedImage) createImage(getWidth(), getHeight());
+        }
+
+        Graphics bufferGraphics = buffer.getGraphics();
+        super.paintComponent(bufferGraphics);
+        for (Character c : field.getCharacters()) {
+            paintCharacter(bufferGraphics, c);
+        }
+        g.drawImage(buffer, 0, 0, null);
+    }
+}
